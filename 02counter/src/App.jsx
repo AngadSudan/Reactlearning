3481d730bd1a35201e8dc7ed154e04537efc23c7
AngadSudan <angadsudan453@gmail.com>
2024-08-02 14:17:34 +0530

    logic of the counter made

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,12 +1,46 @@
-
+import { useState } from 'react'
 function App() {
-  
-
+    // let counter=15;
+    const [counter, setCounter]= useState(0);
+    const add=()=>{
+      console.log("counter value to", Math.random());
+      setCounter(counter+1);
+    }
+    const subtract=()=>{
+      console.log("counter value is", Math.random()); 
+      setCounter(counter-1);
+    }
   return (
     <>
-      
+      <h1>Counter using React</h1>
+      <h2>counter value is {counter}</h2>
+      <button onClick={add}> Increment</button>
+      <button onClick={subtract}>Decrement</button>
     </>
   )
 }
 
-export default App
+export default App;
+// function App() {
+//   // const counter=0;  
+//   const [counter, setcounter]= useState(0);
+//   const add=()=>{
+//     console.log("Value updated to ", counter);
+//     setcounter(counter+1)
+//   }
+//   const minue=()=>{
+//     console.log("value up");
+//     setcounter(counter-1);
+//   }
+//   return (
+//     <>
+//       <h1>Counter Using React</h1>
+//       <h2> Counter value is {counter}</h2>
+//       <button onClick={add}>Add</button>
+//       <br />
+//       <button onClick={minus}>Minus</button>
+//     </>
+//   )
+// }
+
+// export default App
